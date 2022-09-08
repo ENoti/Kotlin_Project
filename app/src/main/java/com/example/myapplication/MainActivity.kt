10b0604caa5101b87ec2e5954d267dcb62fe521c
Saint <dazhuk2016@yandex.ru>
2022-09-08 23:08:37 +0300

    "Все гуд"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var x = 5
         setContentView(R.layout.activity_main)
     }
 }
